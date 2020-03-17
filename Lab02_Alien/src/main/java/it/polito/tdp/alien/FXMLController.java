b45b114101ec1b2cf070c192abb68ec5e3bb0147
francescacatalano <francescacatalano@localhost>
2020-03-17 19:17:03 +0100

    versione finita

--- Lab02_Alien/src/main/java/it/polito/tdp/alien/FXMLController.java
+++ Lab02_Alien/src/main/java/it/polito/tdp/alien/FXMLController.java
@@ -1,40 +1,92 @@
 package it.polito.tdp.alien;
 
 import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.ResourceBundle;
-
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 public class FXMLController {
+	
+	Map<String,String> map = new HashMap<>();
 
     @FXML
     private ResourceBundle resources;
 
     @FXML
     private URL location;
-    
+
+    @FXML
+    private TextField txtInsert;
+
     @FXML
     private Button btnTranslate;
-    
+
+    @FXML
+    private TextArea txtView;
+
     @FXML
     private Button btnReset;
 
     @FXML
-    void doTranslate(ActionEvent event) {
-    	// TODO - add the button and complete this    	
-    }
-    
-    
-    @FXML
     void doReset(ActionEvent event) {
-    	// TODO - add the button and complete this 
+    	txtView.clear();
+    
+
+
     }
+
+    @FXML
+    void doTranslate(ActionEvent event) {
+    	String word = txtInsert.getText();
+    	
+    	if(word.length()==0) {
+    		txtView.appendText("Devi inseire una parola o una trduzione.\n");
+    		return;
+    		}
+    	
+    	
+    	if(word.contains( " "))
+    			{
+    		  String[] words = word.split(" ");
+    		  if(!words[0].matches("[a-zA-Z]*") || !words[1].matches("[a-zA-Z]*"))
+  			{
+  		txtView.appendText("Formato non valido\n");
+  		return;
+  			} 
+  
+    		  this.map.put(words[0], words[1]);
+    			}
+    	else
+    	{	  if(!word.matches("[a-zA-Z]*"))
+			{
+		txtView.appendText("Formato non valido\n");
+		return;
+			}
+    		if(this.map.containsKey(word))
+    		
+    		txtView.appendText(this.map.get(word)+"\n");
+    		else
+    		txtView.appendText("La parola non è presente nel dizionario! Aggiungila!\n");
+    	}
     
+    	
+ 
     
+    	
+
+    }
+
     @FXML
     void initialize() {
+        assert txtInsert != null : "fx:id=\"txtInsert\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert btnTranslate != null : "fx:id=\"btnTranslate\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtView != null : "fx:id=\"txtView\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert btnReset != null : "fx:id=\"btnReset\" was not injected: check your FXML file 'Scene.fxml'.";
 
     }
 }
